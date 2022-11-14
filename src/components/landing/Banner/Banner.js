a7b5b000f0f5a7d2539ccc6ca7c7439f87bfb39c
RusselHarveyM <russelharvey.mercado@cit.edu>
2022-11-15 07:29:54 +0800

    segregate navigation as new component in banner

--- src/components/landing/Banner/Banner.js
+++ src/components/landing/Banner/Banner.js
@@ -1,24 +1,10 @@
 import "./Banner.css";
-import logo from "../../../images/logo.png";
+import Navigation from "./navigation/Navigation";
 
 function Banner() {
   return (
     <div className="wrapper">
-      <nav className="banner_nav">
-        <img
-          src={logo}
-          height="100px"
-          width="100px"
-          alt="techno_together_logo"
-        />
-        <div className="banner_nav_nav">
-          <a href="#">How to Study Together</a>
-          <a href="#">Design a Study Universe</a>
-          <a href="#">Forums</a>
-          <a href="#">About</a>
-          <a href="#">Register Now</a>
-        </div>
-      </nav>
+      <Navigation />
     </div>
   );
 }
---
+++ src/components/landing/Banner/navigation/Navigation.js
@@ -0,0 +1,19 @@
+import logo from "../../../../images/logo.png";
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <nav className="banner_nav">
+      <img src={logo} height="100px" width="100px" alt="techno_together_logo" />
+      <div className="banner_nav_nav">
+        <a href="#">How to Study Together</a>
+        <a href="#">Design a Study Universe</a>
+        <a href="#">Forums</a>
+        <a href="#">About</a>
+        <a href="#">Register Now</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
