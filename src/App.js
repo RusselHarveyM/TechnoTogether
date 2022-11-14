597438b87e4c54ae5f1a9311e96deedefdad3b65
RusselHarveyM <russelharvey.mercado@cit.edu>
2022-11-14 11:25:36 +0800

    initial project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Banner from "./components/landing/Banner/Banner";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
