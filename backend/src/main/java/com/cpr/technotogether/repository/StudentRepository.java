8c8fdd21d61d44c1523e7ddbe27e1777b8e40a16
John Gabriel B. Rago <johngabriel.rago@cit.edu>
2022-11-22 18:15:57 +0800

    Modify student repository

--- backend/src/main/java/com/cpr/technotogether/repository/StudentRepository.java
+++ backend/src/main/java/com/cpr/technotogether/repository/StudentRepository.java
@@ -4,4 +4,6 @@ import com.cpr.technotogether.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<StudentEntity, String> {
+	
+	StudentEntity findByFirstname(String firstname);
 }
