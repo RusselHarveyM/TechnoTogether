91c78f3c9f9de9da0e5b0c8174c7bedbeb6ab45b
John Gabriel B. Rago <johngabriel.rago@cit.edu>
2022-11-22 18:36:25 +0800

    Add methods

--- backend/src/main/java/com/cpr/technotogether/repository/StudentRepository.java
+++ backend/src/main/java/com/cpr/technotogether/repository/StudentRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface StudentRepository extends JpaRepository<StudentEntity, String> {
 	
 	StudentEntity findByFirstname(String firstname);
+	StudentEntity findByUsername(String username);
+	StudentEntity deleteByUsername(String username);
 }
