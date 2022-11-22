13a2acc5fecc4a379208337b094bcea39a90acfe
John Gabriel B. Rago <johngabriel.rago@cit.edu>
2022-11-22 18:05:50 +0800

    Make changes on student entity

--- backend/src/main/java/com/cpr/technotogether/entity/StudentEntity.java
+++ backend/src/main/java/com/cpr/technotogether/entity/StudentEntity.java
@@ -7,88 +7,133 @@ import javax.persistence.*;
 public class StudentEntity {
 
     @Id
+    private String firstname;
+    
+    private String middlename;
+    
+    private String lastname;
+    
+    private int age;
+    
+    private String address;
+    
+    private int phonenumber;
+    
     @Column(name = "username")
     private String username;
-
+    
     @Column(name = "u_password")
     private String password;
-
-    private String firstname;
-
-    private String middlename;
-
-    private String lastname;
-
+    
+    private String email;
+    
+    @Column(name = "u_identification")
+    private String identification;
+    
     private int points;
 
-    @Column(name = "u_organization")
-    private String organization;
+	public StudentEntity(String firstname, String middlename, String lastname, int age, String address, int phonenumber,
+			String username, String password, String email, String identification, int points) {
+		super();
+		this.firstname = firstname;
+		this.middlename = middlename;
+		this.lastname = lastname;
+		this.age = age;
+		this.address = address;
+		this.phonenumber = phonenumber;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.identification = identification;
+		this.points = points;
+	}
 
-    public StudentEntity(){}
+	public String getFirstname() {
+		return firstname;
+	}
 
-    public StudentEntity(String username, String password, String firstname, String middlename, String lastname, int points, String organization) {
-        this.username = username;
-        this.password = password;
-        this.firstname = firstname;
-        this.middlename = middlename;
-        this.lastname = lastname;
-        this.points = points;
-        this.organization = organization;
-    }
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getMiddlename() {
+		return middlename;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setMiddlename(String middlename) {
+		this.middlename = middlename;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getLastname() {
+		return lastname;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 
-    public String getFirstname() {
-        return firstname;
-    }
+	public int getAge() {
+		return age;
+	}
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
+	public void setAge(int age) {
+		this.age = age;
+	}
 
-    public String getMiddlename() {
-        return middlename;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public void setMiddlename(String middlename) {
-        this.middlename = middlename;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    public String getLastname() {
-        return lastname;
-    }
+	public int getPhonenumber() {
+		return phonenumber;
+	}
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
+	}
 
-    public int getPoints() {
-        return points;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setPoints(int points) {
-        this.points = points;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getOrganization() {
-        return organization;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setOrganization(String organization) {
-        this.organization = organization;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIdentification() {
+		return identification;
+	}
+
+	public void setIdentification(String identification) {
+		this.identification = identification;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+    
 }
