08df70ed085557b13cbdeee3e7d658d41208b148
Hardelle Briones <111712687+HardelleBriones@users.noreply.github.com>
2022-11-22 20:49:23 +0800

    Add ChannelMessage Repository

---
+++ backend/src/main/java/com/cpr/technotogether/repository/ChannelMessageRepository.java
@@ -0,0 +1,10 @@
+package com.cpr.technotogether.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cpr.technotogether.entity.ChannelMessageEntity;
+
+public interface ChannelMessageRepository extends JpaRepository<ChannelMessageEntity, Integer>{
+
+}
+ 
