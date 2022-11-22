32943db8ea49f113e8546026ffe2a7032c7453a5
John Gabriel B. Rago <johngabriel.rago@cit.edu>
2022-11-22 18:37:22 +0800

    Add crud methods

--- backend/src/main/java/com/cpr/technotogether/service/StudentService.java
+++ backend/src/main/java/com/cpr/technotogether/service/StudentService.java
@@ -5,6 +5,8 @@ import com.cpr.technotogether.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -15,11 +17,27 @@ public class StudentService {
     public StudentService(StudentRepository srepo) {
         this.srepo = srepo;
     }
-
+    
+    //Create a student record
     public StudentEntity createStudent(StudentEntity student){
         return srepo.save(student);
     }
 
+    //Read a student record by first name
+    public StudentEntity findByFirstname(String firstname){
+        Optional<StudentEntity> result = srepo.findById(firstname);
+
+        StudentEntity studentEntity = null;
+
+        if(result.isPresent()){
+            studentEntity = result.get();
+        }else{
+            throw new RuntimeException("Not found");
+        }
+        return studentEntity;
+    }
+    
+    //Read a student record by username
     public StudentEntity findByUsername(String username){
         Optional<StudentEntity> result = srepo.findById(username);
 
@@ -32,12 +50,43 @@ public class StudentService {
         }
         return studentEntity;
     }
+    
+    //Read all students records from tbl_student
+    public List<StudentEntity> getAllStudents(){
+		return srepo.findAll();
+	}
 
-    public void deleteByUsername(String username){
-        StudentEntity student = findByUsername(username);
-        if(student != null){
-            srepo.delete(student);
-        }
+    //U - Update a student record
+  	@SuppressWarnings("finally")
+  	public StudentEntity putStudent(String username, StudentEntity newStudentDetails) throws Exception{
+  		StudentEntity student = new StudentEntity();
+  		
+  		try { 
+  			//Step 1 -search the id number of the student
+  			student = srepo.findByUsername(username);//findById() is a pre-defined method
+  			
+  			//Step 2 - Update the record
+  			student.setPoints(newStudentDetails.getPoints());
+  			
+  		}catch(NoSuchElementException nex) {
+  			throw new Exception("Username " + username + "does not exist");
+  		}finally {
+  			//Step 3 - Save the the information and return the value
+  			return srepo.save(student);
+  		}
+  	}
+  	
+    public String deleteByUsername(String username){
+        //StudentEntity student = findByUsername(username);
+        String msg;
+        if(srepo.findByUsername(username) != null) {	//step 1 - find the record
+			srepo.deleteByUsername(username); 			//step 2 - delete the record
+			msg = "Student username " + username + " is successfully deleted";
+		}
+		else {
+			msg = "Student username " + username + " is NOT FOUND";
+		}
+		return msg;
     }
 
     public void updateUser(StudentEntity student){
