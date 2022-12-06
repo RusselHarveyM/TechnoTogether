56d26af3f77252618109dd9304eca2d6ed787c80
Russel Harvey Mercado <russelharvey.mercado@cit.edu>
2022-12-06 16:39:36 +0800

    add modal window in forum component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Landing from "./components/landing/Landing";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Home from "./components/home/Home";
-import Register from "./components/register/Register"
+import Register from "./components/register/Register";
 import { useState } from "react";
 
 function App() {
@@ -30,7 +30,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<Landing />} />
-          <Route path="/home/*" element={<Home basename="/home" />} />
+          <Route path="/home/*" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
--- frontend/src/components/forum/forum.js
+++ frontend/src/components/forum/forum.js
@@ -1,10 +1,90 @@
 import React from "react";
 import "./Forum.css";
+import Icon from "@mui/material/Icon";
+import { Link } from "react-router-dom";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "#fff",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function Forum() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [value, setValue] = React.useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <>
-      <h1>Hello Forum</h1>
+      <nav className="forum_nav">
+        <button id="newTopic_Btn" onClick={handleOpen}>
+          <Icon id="addSign">add_circle</Icon>
+          Start New Topic
+        </button>
+        <Link id="discoverSpcs_Btn">Discover Spaces</Link>
+      </nav>
+      <article className="forum_article">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          delectus beatae excepturi reiciendis atque. Aliquam facilis minus
+          earum sed! Laboriosam voluptates dolor eos possimus facere repudiandae
+          mollitia doloribus molestias! Blanditiis! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Officiis dolorum nihil rerum, ipsam qui
+          temporibus ex vitae dolore inventore quaerat. Laudantium facilis
+          distinctio enim. Doloremque minus dignissimos qui officia eveniet?
+        </p>
+      </article>
+      <section className="forum_out_section">Lorem</section>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Add New Topic
+            </Typography>
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Subject"
+              multiline
+              maxRows={4}
+              value={value}
+              onChange={handleChange}
+            />
+            <TextField
+              id="outlined-multiline-static"
+              label="Content"
+              multiline
+              rows={4}
+            />
+          </Box>
+        </Fade>
+      </Modal>
     </>
   );
 }
