9dc855ca5e5f5b01ea839f140b72e7af909bcf28
Queen Riza Montayre <queenriza.montayre@cit.edu>
2022-12-22 19:36:45 +0800

    Modify GoalController class

--- backend/src/main/java/com/cpr/technotogether/controller/GoalController.java
+++ backend/src/main/java/com/cpr/technotogether/controller/GoalController.java
@@ -3,6 +3,7 @@ package com.cpr.technotogether.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,24 +25,28 @@ public class GoalController {
 	GoalService gserv;
 	
 	// Create or insert a goal
+	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping("/postGoal")
 	public GoalEntity insertGoal(@RequestBody GoalEntity goal) {
 		return gserv.insertGoal(goal);
 	}
 	
 	// Read all records
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/getAllGoals")
 	public List<GoalEntity> getAllGoals() {
 		return gserv.getAllGoals();
 	}
 		
 	// Update a goal
+	@CrossOrigin(origins = "http://localhost:3000")
 	@PutMapping("/putGoal")
 	public GoalEntity putGoal(@RequestParam int goalid, @RequestBody GoalEntity newGoalDetails) throws Exception {
 		return gserv.putGoal(goalid, newGoalDetails);
 	}
 		
 	// Delete goal
+	@CrossOrigin(origins = "http://localhost:3000")
 	@DeleteMapping("/deleteGoal/{goalid}")
 	public String deleteGoal(@PathVariable int goalid) {
 		return gserv.deleteGoal(goalid);
